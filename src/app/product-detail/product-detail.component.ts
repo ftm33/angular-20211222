d73a05cab5cca8d11706a9e9b6381be4ec0fbd87
ftm33 <814400646jzd@gmail.com>
2021-12-22 17:37:04 +0900

    2 files modified

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { Product } from '../products';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-
+  @Input() product: Product | undefined;
   constructor() { }
 
   ngOnInit() {
