771156a7416400b6906e63a518fab70e111ce709
ftm33 <814400646jzd@gmail.com>
2021-12-23 17:22:37 +0900

    3 files modified, 3 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +9,8 @@ export class CartService {
   items: Product[] = [];
   addToCart(product: Product) {
     this.items.push(product);
-    console.log(this.items);
+
+    //console.log(this.items);
   }
   getCart() {
     return this.items;
@@ -17,5 +19,11 @@ export class CartService {
     this.items = [];
     return this.items;
   }
-  constructor() {}
+
+  constructor(private http: HttpClient) {}
+  getShippingPrice() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
+  }
 }
